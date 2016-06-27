597786be8efefab936d36569f67c3ff48c705292
cboseak@gmail.com <jamstack05>
2016-06-27 13:11:39 -0500

    initial commit

---
+++ Spinner/Spinner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spinner
+{
+    public class Spinner
+    {
+        public string SpinText(string text)
+        {
+            var words = text.Split(' ');
+            return text;
+        }
+    }
+}
---
+++ WebApplication11/Library/Spinner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using NHunspell;
+
+namespace WebApplication11.Library
+{
+    public class Spinner
+    {
+        public static string SpinText(string text)
+        {
+            var words = text.Split(' ');
+
+            
+            MyThes thes = new MyThes("th_en_US_new.dat");
+            Hunspell hunspell = new Hunspell("en_US.aff", "en_US.dic");
+            for (var i = 0; i < words.Length; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                ThesResult tr = thes.Lookup(words[i], hunspell);
+                if (tr != null)
+                {
+                    foreach (ThesMeaning meaning in tr.Meanings)
+                    {
+                        foreach (string synonym in meaning.Synonyms)
+                        {
+                            
+                            sb.Append(synonym + "|");
+                        }
+                    }
+                }
+                if(sb.ToString().Length > 2)
+                    words[i] = "{" + sb.ToString().Substring(0, sb.ToString().Length - 1) + "}";
+            }
+            text = string.Join(" ", words);
+            return text;
+        }
+    }
+}
